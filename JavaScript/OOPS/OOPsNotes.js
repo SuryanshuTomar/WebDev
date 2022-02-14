3313c88ed1c01b4b0e9b519771e1a2fdd9c28a42
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-02-14 22:13:31 +0530

    Added Dot vs Bracket Notation Notes

--- JavaScript/OOPS/OOPsNotes.js
+++ JavaScript/OOPS/OOPsNotes.js
@@ -5,7 +5,6 @@
 // Storing variables and functions together in one container.
 // We can refer this as an object.
 
-
 // 1. A Class-Based language (like JAVA and C++) have a class construct which defines all the properties that characterize a set of objects
 // 2. A Prototype-Based language has a notion of an object which serves as a template from which we get the properties of a new object.
 // 3. JavaScript by design is not a class-based object-oriented language but a prototype-based language.
@@ -30,8 +29,6 @@
 // console.log(person.name); //Yash Gulati
 // console.log(person.address.city); //Bangalore
 // console.log(person.address.country); //India
-
-
 
 // ---------------------------------------------------------------------------------------------------
 // HOW TO CREATE AN OBJECT?
@@ -76,6 +73,44 @@
 // };
 // bioData.getData();
 
+// -----------------------------------------------------------------------------------------------------------
+// ACCESSING PROPERTIES OF AN OBJECT -
+// There are two ways to access properties on an object:
+// 1. The Dot Notation - Dot notation is used most frequently. Below we’ll dive into each and discover some of the differences.
+// Example:
+// let obj = {
+//   cat: 'meow',
+//   dog: 'woof'
+// };
+// let sound = obj.cat;
+// console.log(sound);
+// // meow
+
+// 2. Bracket Notation - You can access properties on an object by specifying the name of the object followed by the property name in brackets.
+// Example:
+// let obj = {
+//   cat: 'meow',
+//   dog: 'woof'
+// };
+// let sound = obj['cat'];
+// console.log(sound);
+// // meow
+
+// NOTE:
+// Dot notation:
+// - Property identifies can only be alphanumeric (and _ and $)
+// - Property identifiers cannot start with a number.
+// - Property identifiers cannot contain variables.
+// - OK — obj.prop_1, obj.prop$
+// - Not OK — obj.1prop, obj.prop name
+
+// Bracket notation:
+// - Property identifiers have to be a String or a variable that references a String.
+// - It is okay to use variables, spaces, and Strings that start with numbers
+// - OK — obj["1prop"], obj["prop name"]
+
+// More Info:
+// https://medium.com/dailyjs/dot-notation-vs-bracket-notation-eedea5fa8572
 
 // -------------------------------------------------------------------------------
 // WHAT IS "this" OBJECT ?
@@ -103,21 +138,14 @@
 
 // Note : 'this' object will not work with arrow functions. with arrow functions, this object will return the window object and not the current context object.
 
-
 // ---------------------------------------------------------------------------------------------------
-// CONSTRUCTORS - 
-
-
-
+// CONSTRUCTORS -
 
 // 1. We can create objects and initialize its properties by using special function called Constructors.
 // 2. Constructors help us create a blueprint for creating objects.
 // 3. We reference the properties of the object inside the constructor using this keyword -
 //      - A constructor (like a function) can be parameterized or non-parameterized.
 //      - It is written with Capital or PascalCase
-
-
-
 
 //Constructor Function with parameters
 // function Car(make, model, color) {
@@ -127,22 +155,16 @@
 //  this.start = function () {   //"start" function of Car object
 //  console.log("Car Start");
 
-
-
-
 // Constructors - Creating Objects -
 // 1. We can create a new object with a constructor using the new keyword. When used, it does 3 things -
 // 2. It creates a new empty object. {}
-// 3. Sets this to point to the new object created - 
+// 3. Sets this to point to the new object created -
 //      - It will be helpful in setting properties of the new object from inside the constructor
 //      - this -> {}
 // 4. Returns the newly created object from the Constructor function.
 
 // const myCar = new Car("Audi", "Q3", "Black");
 // console.log(myCar.color) //Black
-
-
-
 
 // NOTE => “this keyword points to the object executing the current function.”
 // 1. For an object method ⇒ it points to object executing the method.
@@ -164,12 +186,8 @@
 // In a constructor (or if you call an object using the new operator), this references an empty object in the memory.
 // NOTE => functions inside a class (or/and object in js) is called a method.
 
-
-
 // ---------------------------------------------------------------------------------------------------
-// BIND METHOD - 
-
-
+// BIND METHOD -
 
 // Observe that when we pass the method as callback, the value of this is lost and when the executor function executes the callback, this is not pointing to the student object anymore.
 
@@ -190,8 +208,7 @@
 // //Passing the object method as callback
 // executeObjectFunction(student.greet);
 
-
-// STEPS - 
+// STEPS -
 // 1. What we can do is pass the value of this explicitly to the student.greet method
 // 2. To achieve this we have a method which we can apply on methods → bind
 
@@ -220,8 +237,7 @@
 
 // setTimeout(timer.addTen, 500);
 
-
-// Answer - 
+// Answer -
 // const timer = {
 //   currentTime: 0,
 //   showTime: function () {
@@ -233,24 +249,18 @@
 //   },
 // };
 
-// setTimeout(timer.addTen.bind(timer), 500); 
-
-
-
+// setTimeout(timer.addTen.bind(timer), 500);
 
 // ---------------------------------------------------------------------------------------------------
-// ES6 CLASSES - 
+// ES6 CLASSES -
 
 // 1. JS is not inherently a class-based language, but ES6 provides us with “class-like” syntax which emulates the idea of classes in JavaScript
 // 2. Under the hood, it is doing the same thing we were doing with Constructor functions.
 // 3. Classes are blueprints that describe a particular object.
 // 4. A class is declared with a reserved keyword - class, followed by the class name in Pascal Case (same as constructors)
 
-
-
 // --------------------------------------------------------------------------------------------------
 // HOW TO CREATE JS CLASSES -
-
 
 // 1. Use the class keyword followed with the class name (ex. class Person)
 // 2. Create a constructor method inside the class - can be parameterized/non-parameterized.
@@ -263,7 +273,6 @@
 //   //...other methods
 // }
 
-
 // Question - Create a class method by just writing the method name (ex. sumSalaries()) and omit the function keyword OR use the Arrow function syntax.
 // greet() {
 //   console.log("Hello " + this.name);
@@ -273,20 +282,13 @@
 //   console.log("Hello " + this.name);
 // };
 
-
-
 // ---------------------------------------------------------------------------------------------------
-// INHERITANCE - 
-
-
+// INHERITANCE -
 
 // 1. Class inheritance is a way for one class to extend another class. So we can create new functionality on top of the existing.
 // 2. We can use the extends keyword after the class declaration to "extend" the class.
 // 3. So the inherited class shall have the members of the base class (parent class) as well its own members.
 // 4. This helps us in reducing duplicate code and reuse the already written one.
-
-
-
 
 // CODE -
 
@@ -312,20 +314,13 @@
 //   }
 // }
 
-
-
-
 // ---------------------------------------------------------------------------------------------------
-// METHOD OVERRIDING - 
-
-
+// METHOD OVERRIDING -
 
 // 1. It is a feature that allows a child class to provide a specific implementation of a method already provided by the parent class.
 // 2. If we define the same method in the child class -> the objects of the child class will call that particular implementation instead of the parent class implementation.
 
-
-
-// CODE - 
+// CODE -
 
 // //Base or Parent class
 // class Student {
@@ -339,7 +334,6 @@
 //     return this;
 //   }
 // }
-
 
 // //Derived or Child class
 // class SportsCaptain extends Student {
@@ -359,23 +353,16 @@
 // const babita = new SportsCaptain("Babita");
 // babita.markAttendance(); //Sports Captain Babita is present
 
-
-
-
 // ---------------------------------------------------------------------------------------------------
-// SUPER KEYWORD - 
-
-
+// SUPER KEYWORD -
 
 // If we wish to explicitly call the parent class implementation we can use the super keyword.
 //      1). super.method(...) to call a parent method.
 //      2). super(...) to call a parent constructor (inside child class constructor only).
 
-
-
 // CODE -
 
-//Method Override in Child Class - 
+//Method Override in Child Class -
 // markAttendance() {
 //   super.markAttendance();
 //   console.log("Sports Captain " + this.fullName + " is present");
@@ -388,14 +375,11 @@
 //Yash Gulati is present
 //Sports Captain Yash Gulati is present
 
-
-
-// NOTE => 
+// NOTE =>
 // 1. If we wish to explicitly call the parent class implementation we can use the super keyword.
 // 2. Use super.method(...) to call a parent method.
 
-
-// CODE - 
+// CODE -
 
 //Method Override in Child Class
 // markAttendance() {
@@ -410,21 +394,14 @@
 //Yash Gulati is present
 //Sports Captain Yash Gulati is present
 
-
-
-
 // ---------------------------------------------------------------------------------------------------
-// CONSTRUCTOR OVERLOADING - 
-
-
+// CONSTRUCTOR OVERLOADING -
 
 // If the child class does not have a constructor of its own, it uses the constructor of its parent class.
-// We can have a separate constructor for the child also, 
+// We can have a separate constructor for the child also,
 // BUT then we'll need to use the super keyword inside the child class's constructor and pass all the values needed to initialize the parent class constructor.
 
-
-
-// CODE - 
+// CODE -
 
 //  //Parent class has one parameter - name
 //   constructor(name) {
@@ -439,7 +416,5 @@
 //     //Initializing members of SportsCaptain class
 //     this.age = age;
 //   }
-
-
 
 // ---------------------------------------------------------------------------------------------------
