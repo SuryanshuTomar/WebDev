2ccac53434dd3256569949870d9502dee96af37b
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-01 11:30:59 +0530

    Tailwind setup notes

---
+++ Tailwind-CSS/CWH/Notes.js
@@ -0,0 +1,16 @@
+// => Why use Tailwind CSS ?
+// 1. No Reinventing of class names required.
+// 2. CSS file doesn't grow with your html and designs
+// 3. When you make a change, no risk of breaking existing templates
+
+// => Setting Up Tailwind CSS -
+// 1. Run -> npm init -y
+// 2. Run -> npm install -D tailwindcss postcss autoprefixer vite
+// 3. Run -> npx tailwind init -p
+// 4. Create a css file "input.css", add it to your html and edit with this context :
+//    @tailwind base;
+//    @tailwind components;
+//    @tailwind utilities;
+// 5. In your tailwind.config.js file replace  -> content:[] to content:["*"]
+// 6. Add "start":"vite" to your script in package.json
+// 7. Run npm run start command to start a dev server.
