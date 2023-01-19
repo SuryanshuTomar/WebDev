f5587de4705794d98f7fb72685d9f95e2145889d
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-19 21:07:16 +0530

    scaffolading tailwind css file

--- Tailwind-CSS/Lesson 08 -  Advanced/tailwind.config.js
+++ Tailwind-CSS/Lesson 08 -  Advanced/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ["./build/*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: { xs: "480px" },
+			spacing: {
+				13: "3.25rem",
+			},
+			fontSize: {
+				"10xl": ["9rem", { lineHeight: "1.2" }],
+			},
+		},
 	},
 	plugins: [],
 };
