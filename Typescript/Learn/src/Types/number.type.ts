63606ef6389f9d8b0927a846b04a9c1b824856e4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-05 20:30:19 +0530

    number type

---
+++ Typescript/Learn/src/Types/number.type.ts
@@ -0,0 +1,14 @@
+// Number Type -
+
+let num: number = 100;
+
+// Assigning any kind of number is possible -
+num = 28.7;
+
+num = -200;
+
+num = Infinity * 0.1;
+
+num = parseInt("12"); // parsing the string to number and storing it in "num" variable
+
+// num = "123"; // but we cannot directly assign a string number as auto type cast will happen in runtime and ts will throw error before that in the development phase as it is now allowed
