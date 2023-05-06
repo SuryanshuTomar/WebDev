44ca4976cc97088daf93328ebbe492b710a92550
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-06 11:03:29 +0530

    string type

---
+++ Typescript/Learn/src/Types/string.type.ts
@@ -0,0 +1,10 @@
+// STRING TYPE -
+
+// Declaring and Initializing String
+let f_Name: string = "Mark";
+
+// String Expression
+let fullName: string = f_Name + " " + "Mark";
+
+// String Methods
+let nameComp: string = fullName.toUpperCase();
