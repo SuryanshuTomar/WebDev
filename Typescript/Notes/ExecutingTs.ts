246258bbbdbccf65f81b1b5760ad2ee1f098b02c
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-25 21:23:20 +0530

    add: execution methods

---
+++ Typescript/Notes/ExecutingTs.ts
@@ -0,0 +1,13 @@
+// Method 1 - 
+// 1. Use command - npm install -g typescript to install typescript.
+// 2. Then create a file which we want to execute like index.ts
+// 3. In terminal now run execute the following - 
+//     - tsx index.ts
+//     - node index.js
+
+// Method 2 - 
+// - Using TS-Node - 
+// 1. Use command - npm install -g typescript to install typescript.
+// 2. Use command - npm install -g ts-node to installl the ts-node dependency.
+// 3. In terminal now run execute the following - 
+//   - ts-node index.ts
